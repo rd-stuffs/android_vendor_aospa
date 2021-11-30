654ec18a945fd6e4ea9e45a4694a863c2f0feefa
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-30 08:35:49 +0100

    fonts: Import Linotte font
    * From https://github.com/yaap/vendor_themes/commit/b5502cc6b10f9cb3a61cd7f9778f8a30e62147a4
    
    Change-Id: I57e29d75eb3ed27cb649eacffd30566314322b77
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- themes/fonts/fonts.mk
+++ themes/fonts/fonts.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FontHarmonySansOverlay \
+    FontLinotteSourceOverlay \
     FontManropeOverlay \
     FontMiLanProOverlay \
     FontOnePlusSansOverlay \
