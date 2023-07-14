346708e3e1c10b8953b1ac4a3987836d4a138c19
Richard Raya <rdxzv.dev@gmail.com>
2023-07-14 03:15:39 -0300

    target: Build GameSpace
    Change-Id: Ic98a9338bc2766191fd5d171e38741535925b42c

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, external/google-fonts/lato/fonts.mk)
 
+# Game Space
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Gestures
 PRODUCT_PACKAGES += \
     vendor.aospa.power-service
