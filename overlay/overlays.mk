20ac96b6c194d2d9f44f784410bdca0aa3eba148
ViShal69x <tk5762996@gmail.com>
2024-09-13 19:42:51 +0000

    overlay: Use headline font from framework on PermissionController
    Fixes headline font being stuck to Roboto in Security & Privacy settings.
    
    Change-Id: I2e60d002160d780263a7fe6c6bd7ed7462d1eeb5
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -19,6 +19,8 @@ PRODUCT_PACKAGES += \
     AOSPAImmersiveNavigationOverlay \
     AOSPANetworkStackOverlay \
     AOSPANetworkStackMainlineOverlay \
+    AOSPAPermissionControllerOverlay \
+    AOSPAPermissionControllerMainlineOverlay \
     AOSPASettingsOverlay \
     AOSPASettingsProviderOverlay \
     AOSPASystemUIOverlay \
