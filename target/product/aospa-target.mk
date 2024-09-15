79e52e43182e80fe7748b876d1e0a8235483d9a4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-15 12:38:29 +0530

    target: Remove bootanimation
    It is actually significantly regressing boot times.
    
    Before:
    [   20.232928] Boot completed
    
    After:
    [   11.249714] Boot completed
    
    Change-Id: I5367b24fb12f25bccb5685759a06279f8557836e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -35,9 +35,6 @@ endif
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.config.media_vol_steps=30
 
-# Boot Animation
-$(call inherit-product, vendor/aospa/bootanimation/bootanimation.mk)
-
 # Camera
 PRODUCT_PACKAGES += \
     Aperture
