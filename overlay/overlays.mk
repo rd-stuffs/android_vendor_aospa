258d68bb8ba98b91b648108b87bb3425cb994009
Richard Raya <rdxzv.dev@gmail.com>
2024-12-04 18:50:06 -0300

    fonts: Import RobotoSystem from ProtonAOSP
    Change-Id: Ie2f721a0d879a6fdd195500adad13c2475c905d0
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     FontOppoSansOverlay \
     FontPingFangOverlay \
     FontQuicksandOverlay \
+    FontRobotoOverlay \
     FontSwitzerOverlay \
     FontTwainsixevenOverlay \
     FontUrbanistOverlay \
