285086e94cfea8f6ae423bd35630daec66ef6195
Richard Raya <rdxzv.dev@gmail.com>
2024-12-16 22:34:22 -0300

    Revert "target: Turn on and tweak USAP"
    This reverts commit ec95700de07ad9e0063ba418a3911829482aca5b.
    
    Change-Id: I6174c18fcf38fff9e1f3b96aeca8c7453a760fe3
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -129,14 +129,6 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor
 
-# Properties for the Unspecialized App Process Pool
-PRODUCT_SYSTEM_PROPERTIES += \
-    dalvik.vm.usap_pool_enabled=true \
-    dalvik.vm.usap_refill_threshold=5 \
-    dalvik.vm.usap_pool_size_max=10 \
-    dalvik.vm.usap_pool_size_min=1 \
-    dalvik.vm.usap_pool_refill_delay_ms=3000
-
 # ART/Dex Debug
 ART_BUILD_TARGET_NDEBUG := true
 ART_BUILD_TARGET_DEBUG := false
